b89b830b1cd62c6a94dc56fec498d84616586428
pzeadrian <pzeadrian@duck.com>
2022-09-23 08:18:49 -0700

    Update Windows

--- Windows/git_sync.cmd
+++
@@ -1,9 +0,0 @@
-cd C:\Users\pzeadrian\Documents\configNeovim_lua
-git pull
-
-cp C:\Users\pzeadrian\AppData\Local\nvim\init.lua C:\Users\pzeadrian\Documents\configNeovim_lua\
-cp -R C:\Users\pzeadrian\AppData\Local\nvim\lua\ C:\Users\pzeadrian\Documents\configNeovim_lua\
-
-git add --all
-git commit -m "Update Windows"
-git push
