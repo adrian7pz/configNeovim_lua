2eb17122f3390557cd8f0b8b6fd0d22108fd20c7
pzeadrian <pzeadrian@duck.com>
2022-09-23 08:37:17 -0700

    Update Windows

--- Windows/git_sync.cmd
+++ Windows/git_sync.cmd
@@ -9,6 +9,7 @@ rmdir /s /q lua\
 copy C:\Users\pzeadrian\AppData\Local\nvim\init.lua C:\Users\pzeadrian\Documents\configNeovim_lua\Windows\
 copy C:\Users\pzeadrian\AppData\Local\nvim\git_sync.cmd C:\Users\pzeadrian\Documents\configNeovim_lua\Windows\
 xcopy C:\Users\pzeadrian\AppData\Local\nvim\lua C:\Users\pzeadrian\Documents\configNeovim_lua\Windows\lua\
+xcopy C:\Users\pzeadrian\commands C:\Users\pzeadrian\Documents\configNeovim_lua\Windows\msdos_commands\commands\
 
 git add --all
 git commit -m "Update Windows"
---
+++ Windows/msdos_commands/commands/vconf.cmd
@@ -0,0 +1,1 @@
+cd C:\Users\pzeadrian\AppData\Local\nvim
